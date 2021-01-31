0f61a05ad3d5b810314d1bd102927e89b26451e9
sumit9898 <sumitgeneralmail@gmail.com>
2021-02-01 01:35:21 +0530

    Complete Drum kit project Files

---
+++ index.js
@@ -0,0 +1,60 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var btnInnerHTML = this.innerHTML;
+
+    makeSound(btnInnerHTML);
+    animation(btnInnerHTML);
+    });
+
+}
+
+document.addEventListener("keydown", function(event){
+  makeSound(event.key);
+  animation(event.key);
+});
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "k":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "l":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    default: console.log("Error");
+}
+}
+
+function animation(currentkey){
+  var activeBtn = document.querySelector("."+currentkey);
+  activeBtn.classList.add("pressed");
+
+  setTimeout(function(){
+    activeBtn.classList.remove("pressed");
+  }, 100);
+}
